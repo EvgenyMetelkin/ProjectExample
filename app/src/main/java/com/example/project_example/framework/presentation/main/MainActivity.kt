0837d148ef9c4aa8e7fe9e8266437e657f8227aa
Evgen <metelkin.evgen@yandex.ru>
2021-12-01 20:04:46 +0300

    Create activities and nav graph

--- app/src/main/java/com/example/project_example/framework/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/project_example/framework/presentation/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.project_example
+package com.example.project_example.framework.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project_example.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
