c0edfa3ffcba666bcce98c1ded67870fa4e6bcb2
Evgen <metelkin.evgen@yandex.ru>
2021-12-03 18:40:17 +0300

    Create view for DetailsFragment

--- app/src/main/java/com/example/project_example/framework/presentation/details/DetailsFragment.kt
+++ app/src/main/java/com/example/project_example/framework/presentation/details/DetailsFragment.kt
@@ -1,60 +1,45 @@
 package com.example.project_example.framework.presentation.details
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.project_example.R
+import com.example.project_example.business.domain.model.DataItem
+import com.example.project_example.databinding.FragmentDetailsBinding
+import com.example.project_example.framework.base.navArgsSafe
+import com.squareup.picasso.Picasso
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var fragmentDetailsBinding: FragmentDetailsBinding? = null
+
+    private val args by navArgsSafe<DetailsFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    ): View {
+        fragmentDetailsBinding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return fragmentDetailsBinding!!.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentDetailsBinding?.sourceName?.text = args.dataItem.name
+        fragmentDetailsBinding?.imageView?.run { loadImage(this, args.dataItem.imageUrl) }
+        fragmentDetailsBinding?.description?.text = args.dataItem.description
+        fragmentDetailsBinding?.addToFavorites?.setFill(args.dataItem.getIsFavorite())
     }
+
+
+    private fun loadImage(view: ImageView, url: String?) =
+        Picasso.get()
+            .load(url)
+            .placeholder(R.drawable.shadow)
+            .error(R.drawable.ic_launcher_background)
+            .into(view)
 }
